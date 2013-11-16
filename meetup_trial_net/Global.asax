b91e9ecec874f816f47bc0531a5f807c3661d4af
Ben Stevenson <the.ben.stevenson@gmail.com>
2013-11-16 14:11:18 -0700

    first commit

---
+++ meetup_trial_net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="meetup_trial_net.MvcApplication" Language="C#" %>
---
+++ meetup_trial_net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(meetup_trial_net.Startup))]
+namespace meetup_trial_net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
